026d96f2e8701cb9cfd39c1d68aafbe1226835b2
xxXFreezerXxx <freezerfx512@gmail.com>
2023-08-08 21:34:27 +0200

    Added systems to convert image extension

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const { FFmpeg } = FFmpegWASM;
-const { fetchFile } = FFmpegUtil;
+const dataparser = (data)=>{
+    let datasets=[];
+    let str="";
+    for(let i=0;i<data.length;i++){
+        if(data[i]=="S"){
+            datasets.push(str);
+            str="";
+        }else{
+            str+=data[i];
+        }
+    }
+    return datasets;
+}
 window.onload = async()=> {
     const d = document.getElementById("input");
     const ffmpeg = new FFmpeg();
@@ -7,43 +19,32 @@ window.onload = async()=> {
         const file = event.target.files[0];
         const reader = new FileReader();
         reader.onload=async()=>{
-            const data = reader.result.split("\n");
-            let buffer = new ArrayBuffer(data.length);                                                                                                                                   
+            const data = dataparser(reader.result.split("\r\n"))[0];
+            const tip = document.createElement("p");
+            document.body.appendChild(tip);
+            tip.innerText="Creating raw data...";
+            let buffer = new ArrayBuffer(data.length/2);                                                                                                                                   
             let dv = new DataView(buffer);
-            for(let i=0;i<data.length;i++){                                     
-                dv.setUint8(i, parseInt(data[i],16));
+            for(let i=0;i<data.length/2;i++){    
+                dv.setUint8(i, parseInt(data.charAt(i*2)+data.charAt(i*2+1),16));
             }
-            
             const a = document.createElement("a");
             document.body.appendChild(a);
-            a.style = "display: none";
-            //const fetched =await (await fetch("./test.webp")).arrayBuffer();
             let blob = new Blob([buffer], {type: "image/bmp"});
             const fetched = await blob.arrayBuffer();
             url = window.URL.createObjectURL(blob);
-            const tip = document.createElement("p");
-            tip.innerText="loading ffmpeg..."
-            document.body.appendChild(tip);
             a.href = url;
             a.download = "test";
-            
-            
-            /*
-            await ffmpeg.load();
-            //await ffmpeg.FS("writeFile","test.bmp",new Uint8Array(fetched));
-            await ffmpeg.write("test.bmp",url);
-            console.log(ffmpeg.FS('ls', '/'));
-            //MEMFSに保存がこれでできているのかが分からない、いやbmpファイルに対応していない説
-            const load = await ffmpeg.read("test.bmp");
-            */
             ffmpeg.on("log", ({ type, message }) => {
                 console.log(type+" "+message);
               })
               ffmpeg.on("progress", ({ progress, time }) => {
                 console.log(time+" "+progress);
               })
-            await ffmpeg.load({coreURL:`/node_modules/@ffmpeg/core/dist/umd/ffmpeg-core.js`,log:true});
-            await ffmpeg.writeFile("test.bmp",/*await fetchFile("http://127.0.0.1:5500/test.webp")*/new Uint8Array(fetched));
+              tip.innerText="Loading FFmpeg...";
+            await ffmpeg.load({coreURL:`/assets/core/dist/umd/ffmpeg-core.js`,log:true});
+            tip.innerText="Converting...";
+            await ffmpeg.writeFile("test.bmp",new Uint8Array(fetched));
             await ffmpeg.exec(["-i","test.bmp", "test.webp"]);
             const load = await ffmpeg.readFile("test.webp");
             blob = new Blob([load],{type: "image/webp"});
@@ -51,7 +52,8 @@ window.onload = async()=> {
             const element = document.createElement("img");
             element.setAttribute("src",url);
             document.body.appendChild(element);
-            //element.click();
+            element.download=true;
+            tip.innerText="Completed! Right click to save.";
         }
         reader.readAsText(file);
     })                                                                                         
