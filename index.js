953d19b0ec4e77903619abe8b324c6bd58bc315f
xxXFreezerXxx <freezerfx512@gmail.com>
2023-08-08 17:43:51 +0200

    testing

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-window.onload = function() {
+const { FFmpeg } = FFmpegWASM;
+const { fetchFile } = FFmpegUtil;
+window.onload = async()=> {
     const d = document.getElementById("input");
+    const ffmpeg = new FFmpeg();
     d.addEventListener("change",event =>{
         const file = event.target.files[0];
         const reader = new FileReader();
-        reader.onload=()=>{
+        reader.onload=async()=>{
             const data = reader.result.split("\n");
             let buffer = new ArrayBuffer(data.length);                                                                                                                                   
             let dv = new DataView(buffer);
@@ -14,17 +17,41 @@ window.onload = function() {
             const a = document.createElement("a");
             document.body.appendChild(a);
             a.style = "display: none";
-            var blob = new Blob([buffer], {type: "image/bmp"}),
+            //const fetched =await (await fetch("./test.webp")).arrayBuffer();
+            let blob = new Blob([buffer], {type: "image/bmp"});
+            const fetched = await blob.arrayBuffer();
+            url = window.URL.createObjectURL(blob);
+            const tip = document.createElement("p");
+            tip.innerText="loading ffmpeg..."
+            document.body.appendChild(tip);
+            a.href = url;
+            a.download = "test";
+            
+            
+            /*
+            await ffmpeg.load();
+            //await ffmpeg.FS("writeFile","test.bmp",new Uint8Array(fetched));
+            await ffmpeg.write("test.bmp",url);
+            console.log(ffmpeg.FS('ls', '/'));
+            //MEMFSに保存がこれでできているのかが分からない、いやbmpファイルに対応していない説
+            const load = await ffmpeg.read("test.bmp");
+            */
+            ffmpeg.on("log", ({ type, message }) => {
+                console.log(type+" "+message);
+              })
+              ffmpeg.on("progress", ({ progress, time }) => {
+                console.log(time+" "+progress);
+              })
+            await ffmpeg.load({coreURL:`/node_modules/@ffmpeg/core/dist/umd/ffmpeg-core.js`,log:true});
+            await ffmpeg.writeFile("test.bmp",/*await fetchFile("http://127.0.0.1:5500/test.webp")*/new Uint8Array(fetched));
+            await ffmpeg.exec(["-i","test.bmp", "test.webp"]);
+            const load = await ffmpeg.readFile("test.webp");
+            blob = new Blob([load],{type: "image/webp"});
             url = window.URL.createObjectURL(blob);
             const element = document.createElement("img");
             element.setAttribute("src",url);
             document.body.appendChild(element);
-            const tip = document.createElement("p");
-            tip.innerText="Right click the image to save. 画像を右クリックすると保存ができます。"
-            document.body.appendChild(tip);
-            a.href = url;
-            a.download = "test";
-            //a.click();
+            //element.click();
         }
         reader.readAsText(file);
     })                                                                                         
